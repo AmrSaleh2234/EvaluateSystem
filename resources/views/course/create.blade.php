13bf0519823b3f4263fe90d425a1cf33228f586c
Amr Saleh <52182013+AmrSaleh2234@users.noreply.github.com>
2022-06-16 05:56:42 +0200

    add validation for add course and doctor

--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -1,35 +1,66 @@
 @extends('layout.master',['addCourse'=>''])
 @section('content')
     <div class="container-fluid">
-        <form method="POST" action="{{route('course.store')}}" enctype="multipart/form-data">
-             @csrf
+        <form method="POST" onsubmit='return validateForm()' action="{{route('course.store')}}"
+              enctype="multipart/form-data">
+            @csrf
             <div class="form-group">
                 <label for="course_name">Course Name</label>
                 <input type="text" class="form-control" name="name" id="course_name" aria-describedby="emailHelp"
-                       placeholder="name course">
+                       placeholder="name course" required>
 
             </div>
             <div class="form-group">
                 <label for="description_course">description</label>
                 <input type="text" class="form-control" name="description" id="description_course"
-                       placeholder="description">
+                       placeholder="description" required>
             </div>
             <div class="form-group">
                 <label for="creditHours_course">credit hours</label>
                 <input type="text" class="form-control" name="creditHours" id="creditHours_course"
-                       placeholder="credit hours">
+                       placeholder="credit hours" required>
             </div>
             <div class="form-group">
                 <label for="announce_course">announce</label>
-                <input type="text" class="form-control" name="announce" id="announce_course" placeholder="announce">
+                <input type="text" class="form-control" name="announce" id="announce_course" placeholder="announce" required>
             </div>
             <div class="form-group">
                 <label for="photo_course">photo</label>
-                <input type="file" class="form-control" name="photo" id="photo_course" placeholder="photo">
+                <input type="file" class="form-control" name="photo" id="photo_course" placeholder="photo" required>
             </div>
 
 
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
+    <script>
+        function validateForm() {
+            let x = document.forms["myform"]["name"].value;
+            let y = document.forms["myform"]["description"].value;
+            let z = document.forms["myform"]["creditHours"].value;
+            let e = document.forms["myform"]["announce"].value;
+            let r = document.forms["myform"]["photo"].value;
+
+            if (x == "") {
+                alert("Name must be filled out");
+                return false;
+            }
+            if (y == "") {
+                alert("description must be filled out");
+                return false;
+            }
+            if (z == "") {
+                alert("creditHours must be filled out");
+                return false;
+            }
+            if (e == "") {
+                alert("announce must be filled out");
+                return false;
+            }
+            if (r == "") {
+                alert("photo must be filled out");
+                return false;
+            }
+        }
+    </script>
 @stop
--- resources/views/doctor/create.blade.php
+++ resources/views/doctor/create.blade.php
@@ -4,15 +4,43 @@
         @if($errors->any())
             <div class=" alert alert-danger">{{$errors->first()}}</div>
         @endif
-        <form action="{{route('doctor.store')}}" method="post" class="d-flex flex-column p-5 add_doctor" >
+        <form action="{{route('doctor.store')}}" onsubmit='return validateForm()' method="post" class="d-flex flex-column p-5 add_doctor" >
             @csrf
             <h4>Add Doctor</h4>
-            <input type="text" name="name" placeholder="  Enter Name">
-            <input type="email" name="email" placeholder="  Enter Email">
-            <input type="text" name="username" placeholder="  Enter Username">
-            <input type="password" name="password" placeholder="  Enter Password">
+            <input type="text" name="name" placeholder="  Enter Name" required>
+            <input type="email" name="email" placeholder="  Enter Email" required>
+            <input type="text" name="username" placeholder="  Enter Username" required>
+            <input type="password" name="password" placeholder="  Enter Password" required>
             <button class="btn btn-primary">submit</button>
 
         </form>
     </div>
+    <script>
+
+        function validateForm() {
+            let x = document.forms["myform"]["name"].value;
+            let y = document.forms["myform"]["email"].value;
+            let z = document.forms["myform"]["username"].value;
+            let e = document.forms["myform"]["password"].value;
+
+
+            if (x == "") {
+                alert("Name must be filled out");
+                return false;
+            }
+            if (y == "") {
+                alert("description must be filled out");
+                return false;
+            }
+            if (z == "") {
+                alert("creditHours must be filled out");
+                return false;
+            }
+            if (e == "") {
+                alert("announce must be filled out");
+                return false;
+            }
+
+        }
+    </script>
 @stop
